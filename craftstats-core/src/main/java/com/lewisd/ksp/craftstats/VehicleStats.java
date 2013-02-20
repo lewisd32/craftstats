c8b2cd65499ab1f77868defd215651cef5436cd7
Derek Lewis <derek@lewisd.com>
2013-02-19 19:09:33 -0800

    Issue 4: Cleaning up VehicleStats output for stages with no active engines.

--- craftstats-core/src/main/java/com/lewisd/ksp/craftstats/VehicleStats.java
+++ craftstats-core/src/main/java/com/lewisd/ksp/craftstats/VehicleStats.java
@@ -76,45 +76,41 @@ public class VehicleStats extends AbstractVehicleEnvironmentMain {
     }
 
     private void printStageStats(final PrintStream out, final Stage stage) {
-        final int stageNumber = stage.getNumber();
         final Environment environment = stage.getEnvironment();
 
         System.out.printf("  %s @ %.3fkm%n", environment.getName(), stage.getAltitude());
 
+        final double totalStageDryMass = stage.getDryMass();
+        final double totalStageMass = stage.getMass();
+
+        out.printf("  Mass: %.3f t (%.3f t dry)%n", totalStageMass, totalStageDryMass);
+
         final Collection<Engine> engines = stage.getActiveEngines();
-        if (stageNumber != 0) {
-            if (engines.isEmpty()) {
-                out.println("  No active engines");
-                return;
-            }
-
-            if (stage.getMass() == stage.getDryMass()) {
-                return;
-            }
-
-            if (!stage.launchClampsReleased()) {
-                return;
-            }
+        if (!stage.launchClampsReleased()) {
+            out.println("  Launch clamps applied.");
+            return;
         }
 
-        out.println("  Active engines:");
-        for (final Engine engine : engines) {
-            final VehiclePart vehiclePart = engine.getVehiclePart();
-            final double mass = vehiclePart.getMass();
-            final double maxThrust = engine.getMaxThrust();
-            final double atmoIsp = engine.getAtmoIsp();
-            final double vacuumIsp = engine.getVacuumIsp();
-            out.printf("    %s", vehiclePart.getPart().getTitle());
-            if (verbose) {
-                out.printf(" (%s) : %.3f t (maxThrust=%.1f, ispa=%.1f, ispv=%.1f)", vehiclePart.getId(), mass, maxThrust, atmoIsp, vacuumIsp);
+        if (engines.isEmpty()) {
+            out.println("  No active engines.");
+        } else {
+            out.println("  Active engines:");
+            for (final Engine engine : engines) {
+                final VehiclePart vehiclePart = engine.getVehiclePart();
+                final double mass = vehiclePart.getMass();
+                final double maxThrust = engine.getMaxThrust();
+                final double atmoIsp = engine.getAtmoIsp();
+                final double vacuumIsp = engine.getVacuumIsp();
+                out.printf("    %s", vehiclePart.getPart().getTitle());
+                if (verbose) {
+                    out.printf(" (%s) : %.3f t (maxThrust=%.1f, ispa=%.1f, ispv=%.1f)", vehiclePart.getId(), mass, maxThrust, atmoIsp, vacuumIsp);
+                }
+                out.println();
             }
-            out.println();
         }
 
         final double g = environment.getGravity(stage.getAltitude());
 
-        final double totalStageDryMass = stage.getDryMass();
-        final double totalStageMass = stage.getMass();
         final double totalStageThrust = stage.getMaxThrust();
         final double totalStageIsp = stage.getIsp();
         final double burnedFuelMass = stage.getLOXFuelMass() + stage.getSolidFuelMass();
@@ -122,25 +118,26 @@ public class VehicleStats extends AbstractVehicleEnvironmentMain {
         final double twWet = totalStageThrust / (g * totalStageMass);
         final double twDry = totalStageThrust / (g * totalStageDryMass);
 
-        out.printf("  Mass: %.3f t (%.3f t dry)%n", totalStageMass, totalStageDryMass);
-        out.printf("  Max thrust = %.1f kN%n", totalStageThrust);
-        out.printf("  Isp = %.1f m/s%n", totalStageIsp);
-        out.printf("  Delta V = %.1f m/s%n", stage.getDeltaV());
-        out.printf("  T/W wet = %.2f%n", twWet);
-        out.printf("  T/W dry = %.2f%n", twDry);
-        out.printf("  Burned fuel = %.1f t%n", burnedFuelMass);
+        if (!engines.isEmpty()) {
+            out.printf("  Max thrust = %.1f kN%n", totalStageThrust);
+            out.printf("  Isp = %.1f m/s%n", totalStageIsp);
+            out.printf("  Delta V = %.1f m/s%n", stage.getDeltaV());
+            out.printf("  T/W wet = %.2f%n", twWet);
+            out.printf("  T/W dry = %.2f%n", twDry);
+            out.printf("  Burned fuel = %.1f t%n", burnedFuelMass);
 
-        final double fuelFlowRate = (totalStageThrust / 9.81) / totalStageIsp;
-        out.printf("  Fuel flow rate = %.3f t/s%n", fuelFlowRate);
+            final double fuelFlowRate = (totalStageThrust / 9.81) / totalStageIsp;
+            out.printf("  Fuel flow rate = %.3f t/s%n", fuelFlowRate);
 
-        final double burnDuration = burnedFuelMass / fuelFlowRate;
-        out.printf("  100%% thrust burn duration = %.1f s%n", burnDuration);
+            final double burnDuration = burnedFuelMass / fuelFlowRate;
+            out.printf("  100%% thrust burn duration = %.1f s%n", burnDuration);
 
-        final double accelWet = totalStageThrust / totalStageMass;
-        final double accelDry = totalStageThrust / totalStageDryMass;
-        out.printf("  Acceleration         = %.1f m/s/s (%.1f m/s/s dry)%n", accelWet, accelDry);
-        out.printf("  Acceleration after G = %.1f m/s/s (%.1f m/s/s dry)%n", (accelWet - g), (accelDry - g));
-        out.printf("  G-force              = %.1f G's (%.1f G's dry)%n", (accelWet - g) / 9.81, (accelDry - g) / 9.81);
+            final double accelWet = totalStageThrust / totalStageMass;
+            final double accelDry = totalStageThrust / totalStageDryMass;
+            out.printf("  Acceleration         = %.1f m/s/s (%.1f m/s/s dry)%n", accelWet, accelDry);
+            out.printf("  Acceleration after G = %.1f m/s/s (%.1f m/s/s dry)%n", (accelWet - g), (accelDry - g));
+            out.printf("  G-force              = %.1f G's (%.1f G's dry)%n", (accelWet - g) / 9.81, (accelDry - g) / 9.81);
+        }
     }
 
 }
